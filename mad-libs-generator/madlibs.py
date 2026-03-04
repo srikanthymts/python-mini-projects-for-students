040f4da1fa8a623280e8e4de666ec27daac5c970
Takeoff Edu Group <srikanthymts@gmail.com>
2026-03-04 18:12:14 +0530

    Add basic Mad Libs generator script

---
+++ mad-libs-generator/madlibs.py
@@ -0,0 +1,12 @@
+# Mad Libs Generator
+
+print("Welcome to Mad Libs!")
+
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+story = f"Today I saw a {adjective} {noun} that decided to {verb} in the middle of the park!"
+
+print("\nHere is your story:")
+print(story)
